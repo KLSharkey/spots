180204a516e9c49db9a04d09508d65289398aa4c
JYlag <joeylagan@gmail.com>
2018-02-05 12:35:24 -0800

    Fixed LoginController Methods

--- src/main/java/com/liftoff/spots/controllers/LoginController.java
+++ src/main/java/com/liftoff/spots/controllers/LoginController.java
@@ -3,6 +3,7 @@ package com.liftoff.spots.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
@@ -10,16 +11,16 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value="login")
 public class LoginController {
 
-    @RequestMapping(value="signup")
-    public String index(Model model) {
+    @RequestMapping(value="signup", method = RequestMethod.GET)
+    public String displaySignUpPage(Model model) {
 
         model.addAttribute("title", "Spots");
 
         return "login/userSignUp";
     }
 
-    @RequestMapping(value="existing")
-    public String login(Model model) {
+    @RequestMapping(value="existing", method = RequestMethod.GET)
+    public String displayLoginPage(Model model) {
 
         model.addAttribute("title", "Spots");
 
