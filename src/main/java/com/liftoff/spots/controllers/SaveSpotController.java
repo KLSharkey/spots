e8a1f0f3cdefdecea4fa3f2f696e4212462d25a1
Kristen Sharkey <kristen.l.sharkey@gmail.com>
2018-02-28 11:36:15 -0800

    Current location functionality finished

--- src/main/java/com/liftoff/spots/controllers/SaveSpotController.java
+++ src/main/java/com/liftoff/spots/controllers/SaveSpotController.java
@@ -14,7 +14,8 @@ import java.util.HashSet;
 
 
 @Controller
-@RequestMapping(value = "")
+//@RequestMapping(value = "")
+@RequestMapping(value = "geolocation")
 public class SaveSpotController {
 
     @Autowired
@@ -23,6 +24,7 @@ public class SaveSpotController {
         //@Autowired
         //private com.liftoff.spots.models.data.userDao userDao;
 
+        //@RequestMapping(value = "getLocation", method = RequestMethod.GET)
         @RequestMapping(value = "getLocation", method = RequestMethod.GET)
         public String displayLocation(Model model) {
 
@@ -32,8 +34,10 @@ public class SaveSpotController {
             return "geolocation/getLocation";
         }
 
-    @RequestMapping(value = "test")
-    public @ResponseBody String processForm(@RequestBody Spot jsonObject) {
+    //@RequestMapping(value = "test")
+    @RequestMapping(value = "getLocation/current", method = RequestMethod.POST)
+    //public @ResponseBody String processFormCurrentSpot(@RequestBody Spot jsonObject) {
+    public @ResponseBody String processFormCurrentSpot(@RequestBody Spot jsonObject) {
             double latitude = jsonObject.getLatitude();
             double longitude = jsonObject.getLongitude();
             /*String spotName = jsonObject.getSpotName();
@@ -44,9 +48,28 @@ public class SaveSpotController {
             System.out.println(spotInfo);*/
 
             spotDAO.save(jsonObject);
+            System.out.println("here");
+            return "";
 
-            return "test";
+    }
+
+
+    /*@RequestMapping(value = "notCurrent", method=RequestMethod.POST)
+    public @ResponseBody String processFormNotCurrent(@RequestBody Spot jsonObject) {
+        double latitude = jsonObject.getLatitude();
+        double longitude = jsonObject.getLongitude();
+            /*String spotName = jsonObject.getSpotName();
+            String spotInfo = jsonObject.getSpotInfo();
+            System.out.println(longitude);
+            System.out.println(latitude);
+            System.out.println(spotName);
+            System.out.println(spotInfo);*/
+
+        //spotDAO.save(jsonObject);
+
+        //return "spotSaved";
 
     }
 
-    }
+
+
